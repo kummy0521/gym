415e73e90f5a5f37f1e0e936e66f39d786fcc0d2
Kumiko <aoisora.hoshinoyoru.aoiumi@gmail.com>
2025-06-26 21:44:04 +0900

    7日目_お問い合わせページ作成

--- front-page.php
+++ front-page.php
@@ -25,6 +25,50 @@
     <!-- News -->
 	<section class="top_news section">
 		<h2 class="section_title">News</h2>
+<div class="news_archive">
+	<?php
+	$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+
+	$args = array(
+		'post_type'      => 'news',
+		'posts_per_page' => 10,
+		'orderby'        => 'menu_order',
+		'order'          => 'ASC',
+		'paged'          => $paged
+	);
+
+	$news_query = new WP_Query($args);
+	?>
+
+	<?php if ($news_query->have_posts()) : ?>
+		<?php while ($news_query->have_posts()) : $news_query->the_post(); ?>
+			<div class="news_item">
+				<a href="<?php the_permalink(); ?>" class="news_link">
+					<time class="news_date" datetime="<?php echo get_the_date('Y.m.d'); ?>">
+						<?php echo get_the_date('Y.m.d'); ?>
+					</time>
+					<p class="news_text"><?php the_title(); ?></p>
+				</a>
+			</div>
+		<?php endwhile; ?>
+
+		<!-- ページネーション -->
+		<div class="pagination">
+			<?php
+			echo paginate_links(array(
+				'total'   => $news_query->max_num_pages,
+				'current' => $paged,
+				'prev_text' => '&laquo;',
+				'next_text' => '&raquo;',
+			));
+			?>
+		</div>
+
+		<?php wp_reset_postdata(); ?>
+	<?php else : ?>
+		<p>現在、ニュースはありません。</p>
+	<?php endif; ?>
+</div>
 
 		<div class="button_area">
 			<a href="<?= home_url(); ?>/news" class="button">全てのお知らせを見る</a>
@@ -34,6 +78,43 @@
 	<!-- Catalog -->
 	<section class="top_catalog section">
 		<h2 class="section_title">Catalog</h2>
+
+			<div class="catalog_inner">
+				<div class="catalog_archive">
+					<?php
+					$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+					$args = array(
+						'post_type' => 'catalog', // 投稿タイプを指定
+						'posts_per_page' => 16, // 1ページあたりの表示記事数を16に設定
+						'paged' => get_query_var('paged') ? get_query_var('paged') : 1, // 現在のページ番号
+						'orderby' => 'menu_order', // 管理画面の表示順（メニューオーダー）に従って並び替え
+						'order' => 'ASC' // 昇順に並び替え
+					);
+					$the_query = new WP_Query($args);
+
+					if ($the_query->have_posts()) {
+						while ($the_query->have_posts()) {
+							$the_query->the_post();
+
+							// 画像取得
+							$tax_image = get_the_post_thumbnail($post->ID, 'full', array('class' => 'catalog_thumb'));
+							// タイトル
+							$tax_title = get_the_title();
+							// 説明文
+							$tax_desc = get_field('product');
+					?>
+							<div class="catalog_item">
+								<a href="<?php echo get_permalink(); ?>" class="catalog_link">
+									<?php echo $tax_image; ?>
+									<h3 class="catalog_name"><?php echo $tax_title; ?></h3>
+									<div class="catalog_desc"><?php echo $tax_desc; ?></div>
+								</a>
+							</div>
+						<?php
+						}
+						?>
+				</div>
+
 
 		<div class="button_area">
 			<a href="<?= home_url(); ?>/news" class="button">全ての商品を見る</a>
